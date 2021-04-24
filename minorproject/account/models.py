6a895378fe5b0f2b8828ce3d2471b36e070be7ec
pranjaljhala <pranjaljhala22@gmail.com>
2021-04-24 20:45:04 +0530

    account work competed*_*

--- minorproject/account/models.py
+++ minorproject/account/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    GENDER_CHOICE = (('M','MALE'),('F','FEMALE'))
+    
+    gender = models.TextField(max_length=50, choices = GENDER_CHOICE ,blank=True)
+    birth_date = models.DateField(auto_now_add=False,auto_now=False, blank=True,null=True)
+    
+    #profile_pic = models.ImageField(default='profile1.jpg',null=True,blank=True)
+
+    def __str__(self):
+        return self.user.first_name
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- minorproject/account/views.py
+++ minorproject/account/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['pass2']
+
+        user = auth.authenticate(username = email,password = password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Credentials *_*')
+            return render(request,'account/login.html')
+    else:
+        return render(request,'account/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def signup(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']        
+        username = request.POST['email']
+        if User.objects.filter(username = username).exists():
+            messages.info(request,'An Account with this email already exist *_*')
+            return render(request,'account/register.html')
+        if len(username)==0:
+            messages.info(request,'No Username entered _')
+            return render(request,'account/register.html')
+        
+        email = request.POST['email']
+        password = request.POST['pass2']
+        user = User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+        user.profile.gender = getGender(request.POST['gender'])
+        fi = getDate(request.POST['birthday'])
+        user.profile.birth_date = fi
+        user.save()
+        
+        return render(request,'account/login.html')
+    else:
+        return render(request,'account/register.html')
+
+
+
+def getDate(s):
+    sp = s.split('/')
+    sp.reverse()
+    listToStr = '-'.join([str(elem) for elem in sp])
+    return listToStr
+
+def getGender(s):
+    if s == 'Male':
+        return 'M'
+    else:
+        return 'F'
