4c4feba5bcfdb4a40b9b7a1cbc3cd180f3c3d3fa
unknown <rishijoshi2sep@gmail.com>
2021-06-04 14:07:39 +0530

    99.9999% done

--- minorproject/account/urls.py
+++ minorproject/account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .import views
 
 urlpatterns = [
@@ -10,4 +11,21 @@ urlpatterns = [
     path("invoices/",views.invoices, name='invoices'),
     path("address/",views.address, name='address'),
     path("edit_address/",views.edit_address, name='edit_address'),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="account/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_done.html"), 
+        name="password_reset_complete"),
+
 ]
